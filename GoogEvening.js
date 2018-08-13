8cc75ae5b2a890dacef1c2ae3e8b8e440f25bcec
secmeant <patryk.wlazlyn@gmail.com>
2018-08-13 13:39:43 +0200

    Adding first version. Additional features like videos , images and stuff are not well colored

---
+++ GoogEvening.js
@@ -0,0 +1,56 @@
+function setChildBg(parent, color)
+{
+	for ( var i = 0; i < parent.length; i++ )
+	{
+		parent[i].style.backgroundColor = color
+	}
+}
+
+function setChildCl(parent, color)
+{
+	for ( var i = 0; i < parent.length; i++ )
+	{
+		parent[i].style.color = color
+	}
+}
+
+var color_dark = '#101010';
+var color_light = '#ffffff';
+
+document.body.style.backgroundColor = color_dark
+
+// Main div with search results
+document.getElementById('rcnt').style.backgroundColor = "#101010";
+document.getElementById('rcnt').style.color = "#ffffff";
+
+// Div with speed of search results
+document.getElementById('topabar').style.backgroundColor = "#101010";
+document.getElementById('resultStats').style.color = "#ffffff";
+
+// Make all the links white color to be seen on dark background
+setChildCl(document.getElementsByTagName("a"), color_light);
+
+// Divs around search bar
+document.getElementById('hdtbSum').style.backgroundColor = color_dark;
+
+// For now there is only one div that uses this class so there is no problem
+// If somethings breaks it might be here. Just sayin.
+document.getElementsByClassName('sfbgg')[0].style.backgroundColor = color_dark;
+
+setChildBg(document.getElementsByClassName('fbar'), color_dark);
+
+setChildCl(document.getElementsByClassName('gb_X'), color_dark);
+
+// Top stories part
+setChildBg(document.getElementsByTagName('g-card-section'), '#161616');
+
+// For some reason this doesnt want to work
+// leaving it to solve it later
+//var iconSmalls = document.getElementsByClassName('gb_b');
+//for ( var i = 0; i < iconSmalls; i++ )
+//{
+//	iconSmalls[i].style.backgroundColor = color_light;
+//}
+//delete iconSmalls;
+
+console.log('Goog Evening !');
